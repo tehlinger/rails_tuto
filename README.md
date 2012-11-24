27858dd13f2b99826b305feac738a5e7e550c85b
HarnoRanaivo <kshegzyaj@gmail.com>
2012-11-24 12:46:51 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+RAILS_TUTO
+=========
+
+Plop.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'dfa35d7555aad8fc52c782caf8c5249d812a65f98654184ac0ae3ed0055877c6e81cb6def92ec7e6244a762029143461d30fea43324b1d39d9ac8cff2197064d'
